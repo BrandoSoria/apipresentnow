f53c301d93f2a210b43bf28f47c661e39db283f7
brandon <webarebrandy@gmail.com>
2024-04-16 22:23:17 -0600

    dep pug

--- app.js
+++ app.js
@@ -3,48 +3,39 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors'); // Agregar el middleware CORS
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// Configuración del motor de vistas como Pug
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
-// Establecer el entorno de la aplicación como producción
-app.set('env', 'production');
-
-// Middleware para el registro simplificado (comentar para desactivar)
-// app.use(logger('dev'));
-
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware CORS
-app.use(cors());
-
-// Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Middleware para manejar errores 404
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Middleware para manejar errores generales
+// error handler
 app.use(function(err, req, res, next) {
-  // Renderizar página de error con un mensaje genérico
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', {
-    message: 'Ha ocurrido un error en el servidor',
-    error: {}
-  });
+  res.render('error');
 });
 
 module.exports = app;
