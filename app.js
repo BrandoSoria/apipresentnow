5cd61cb326c6868ae4c556f22b8377d87a357b59
brandon <webarebrandy@gmail.com>
2024-04-25 20:58:25 -0600

    nuevo produccion con todo

--- app.js
+++ app.js
@@ -3,39 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const Alumno = require('./Modulos/Alumnos');// Exporte desde Modulos
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const bodyParser = require ('body-parser');
+const Alumno = require('./routes/routes');// Exporte desde Modulos
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//usar node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded ({
+   extended: true
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+}));
+routes(app);
+ //iniciar
+ const server = app.listen(port,(error)=>{
+    if (error) return console.log('error: ${error}');
+    console.log('el servidor escucha en el puerto ${server.addres().port}');
+ });
