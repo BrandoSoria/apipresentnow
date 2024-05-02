4be9482a43cbaa691e14430b308c53e881574935
Ulises <xnuch5@gmail.com>
2024-05-02 11:56:59 -0600

    corregido

---
+++ routes/routes.js
@@ -0,0 +1,109 @@
+const { response, request } = require('express');
+const pool = require('../data/config');
+const router = app => {
+    app.get('/', (request, response) => {
+        response.send({
+            message: '¡Bienvenido a la API REST de Node.js Express!'
+        });
+    });
+
+    // Endpoints para la tabla Profesores
+    app.get('/profesores', (request, response) => {
+        pool.query('SELECT * FROM Profesores', (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    app.get('/profesores/:rfc', (request, response) => {
+        const rfc = request.params.rfc;
+        pool.query('SELECT * FROM Profesores WHERE RFC = ?', rfc, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    // Endpoints para la tabla Departamento
+    app.get('/departamentos', (request, response) => {
+        pool.query('SELECT * FROM Departamento', (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    app.get('/departamentos/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('SELECT * FROM Departamento WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    // Endpoints para la tabla Materia
+    app.get('/materias', (request, response) => {
+        pool.query('SELECT * FROM Materia', (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    app.get('/materias/:clave', (request, response) => {
+        const clave = request.params.clave;
+        pool.query('SELECT * FROM Materia WHERE ClaveMateria = ?', clave, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    // Endpoints para la tabla Plan de Estudio
+    app.get('/planesestudio', (request, response) => {
+        pool.query('SELECT * FROM PlanEstudio', (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    app.get('/planesestudio/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('SELECT * FROM PlanEstudio WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    // Endpoints para la tabla Alumno
+    app.get('/alumnos', (request, response) => {
+        pool.query('SELECT * FROM Alumno', (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    app.get('/alumnos/:numerocontrol', (request, response) => {
+        const numerocontrol = request.params.numerocontrol;
+        pool.query('SELECT * FROM Alumno WHERE NumeroControl = ?', numerocontrol, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    // Endpoints para la tabla Grupo
+    app.get('/grupos', (request, response) => {
+        pool.query('SELECT * FROM Grupo', (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    app.get('/grupos/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('SELECT * FROM Grupo WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+
+    return app;
+};
+
+module.exports = router;
