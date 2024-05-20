5384e2be9b60f5d9346958e47eb93deac96c1406
brandon <webarebrandy@gmail.com>
2024-05-19 23:21:53 -0600

    ajuste de la conexion de la bd para evitar el cierre de la bd

--- routes/conexion.js
+++ routes/conexion.js
@@ -4,20 +4,24 @@ const dotenv = require('dotenv');
 // Cargar variables de entorno
 dotenv.config();
 
-// Configurar la conexión a la base de datos
-const db = mysql.createConnection({
+// Configurar el pool de conexiones a la base de datos
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-});
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+}).promise();
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
     if (err) {
-        console.error('Error al conectar a la base de datos:', err);
+        console.error('Error al conectar al pool de la base de datos:', err);
     } else {
-        console.log('Conectado a la base de datos');
+        console.log('Conectado al pool de la base de datos');
+        connection.release(); // Liberar la conexión de vuelta al pool
     }
 });
 
-module.exports = db;
+module.exports = pool;
